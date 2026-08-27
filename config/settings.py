5a9f281ea43e31bca00bfe789709e830ac68a920
Madhav-001 <madymass2001@gmail.com>
2026-08-27 20:39:19 +0530

    feat: add django-cors-headers to project and configure CORS settings

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-CHANGE_ME_TO_SOMETHING_LONGER_THAN_32_BYTES"
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
